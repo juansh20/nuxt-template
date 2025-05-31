a19ab9b517e9c8f9780d0afff3e944280e5a9261
Juan Eduardo Herrera <juansh.herrera@gmail.com>
2025-05-30 21:37:31 -0500

    tested icon

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
     <h1 class="text-3xl font-bold underline text-red-500">Hello world!</h1>
-    <button class="btn btn-secondary">Secondary</button>
+    <button class="btn btn-secondary">Secondary <Icon name="tabler:alert-triangle-filled" /></button>
   </div>
 </template>
